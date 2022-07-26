daa77e700beab25a3c97e16a32b6e6db40358cb6
cowrycode <josephorji03@gmail.com>
2022-07-26 16:09:04 -0300

    lucky pot random select implemented

--- lib/pages/custompages/canva/luckpot_view.dart
+++ lib/pages/custompages/canva/luckpot_view.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 
@@ -11,94 +13,77 @@ class LuckPot extends StatefulWidget {
 class _LuckPotState extends State<LuckPot> {
   int _value = 0;
   bool _activated = false;
+  final Duration timerTastoPremuto = Duration(days: 0,hours: 0, minutes: 10);
+
+
+  @override
+  void initState() {
+    randomize();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      child: LuchMatrics()
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: LuchMatrics());
+  }
+
+  Widget LuchMatrics() {
+    return Wrap(
+      children: List<Widget>.generate(
+        20,
+        (int index) {
+          return _value == index && _activated == true
+              ? IconButton(
+                  key: Key("$index"),
+                  icon: const Icon(Icons.call),
+                  color: Theme.of(context).colorScheme.secondary,
+                  onPressed: () {},
+                )
+              : ChoiceChip(
+                  key: Key('$index'),
+                  elevation: 6.0,
+                  backgroundColor: Colors.white,
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  //  label: Text('Item $index'),
+                  label: Text('Item'),
+                  selected: _value == index,
+                  onSelected: (bool selected) {
+                    setState(() {
+                      _value = selected ? index : null;
+                     // _activated = index == 10 ? true : false;
+                      print("Selected Value if $_value");
+                    });
+                  },
+                );
+        },
+      ).toList(),
     );
   }
 
-  Widget LuchMatrics(){
-  return Wrap(
-    children: List<Widget>.generate(
-      20,
-          (int index) {
-        return index == 10 && _activated == true?
-        IconButton(
-          key: Key("$index"),
-          icon: const Icon(Icons.call),
-          color: Theme.of(context).colorScheme.secondary,
-          onPressed: () {
-          },
-        ):
-        ChoiceChip(
-          key: Key('$index'),
-          elevation: 6.0,
-          backgroundColor:  Colors.white,
-          padding: const EdgeInsets.only(left :10, right : 10),
-        //  label: Text('Item $index'),
-          label: Text('Item'),
-          selected: _value == index,
-          onSelected: (bool selected) {
+  void randomize(){
+    int _start = timerTastoPremuto.inMilliseconds;
+    Random random = new Random();
+    int randomNumber = random.nextInt(100);
+    const oneDecimal = const Duration(milliseconds: 1000);
+    Timer _timer = new Timer.periodic(
+        oneDecimal,
+            (Timer timer) =>
             setState(() {
-              _value = selected ? index : null;
-              _activated = index == 10 ? true : false;
-              print("Selected Value if $_value");
-            });
-          },
-        );
-      },
-    ).toList(),
-  );
-  //   return Wrap(
-  //     children: [
-  //       thechipchoices(1),
-  //       thechipchoices(2),
-  //       thechipchoices(3),
-  //       thechipchoices(4),
-  //       thechipchoices(5),
-  //       thechipchoices(6),
-  //       thechipchoices(7),
-  //       thechipchoices(8),
-  //       thechipchoices(9),
-  //       thechipchoices(10),
-  //       thechipchoices(11),
-  //       thechipchoices(12),
-  //       thechipchoices(13),
-  //       thechipchoices(14),
-  //       thechipchoices(15),
-  //       thechipchoices(16),
-  //       thechipchoices(17),
-  //       thechipchoices(18),
-  //       thechipchoices(19),
-  //       thechipchoices(20),
-  //       thechipchoices(21),
-  //       thechipchoices(22),
-  //       thechipchoices(23),
-  //     ],
-  //   );
+              _value = random.nextInt(23);
+              print('Timer is : $_start  Index Value is $_value');
+              if (_start == 500000) {
+                _activated = true;
+                timer.cancel();
+              } else {
+                _start = _start - 400;
+              }
+              // if (_start < 100) {
+              //   timer.cancel();
+              // } else {
+              //   _start = _start - 400;
+              // }
+            }));
+  }
 }
-
-Widget thechipchoices(int index){
-  return ChoiceChip(
-    elevation: 6.0,
-    backgroundColor:  Colors.white,
-    padding: const EdgeInsets.only(left :10, right : 10),
-    //  label: Text('Item $index'),
-    label: Text('Item'),
-    selected: _value == index,
-    onSelected: (bool selected) {
-      setState(() {
-        _value = selected ? index : null;
-        print("Selected Value if $_value");
-      });
-    },
-  );
-}
-
-}
-
-
